2c0f39e72cdbf803352c2c9291f5076f7f15fc45
Ensismensi <ensismensi1@gmail.com>
2017-07-21 18:51:50 +0200

    s

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ message.author.send({embed});
 }
 
 if (message.content.startsWith(prefix + 'eval')) {
-  if(message.author.id !== "161860589243727872") return;
+  if (message.author.id !== "161860589243727872") return;
   try {
     var code = args.join(" ");
     var evaled = eval(code);
