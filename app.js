9a5ffc459b26e198667a60fd81177991a110adb5
Ensismensi <ensismensi1@gmail.com>
2017-07-21 17:46:04 +0200

    Update

--- app.js
+++ app.js
@@ -53,6 +53,29 @@ message.author.send({embed});
   message.channel.send({embed});
 }
 
+if (message.content.startsWith(prefix + 'eval')) {
+  if(message.author.id !== "161860589243727872") return;
+  try {
+    var code = args.join(" ");
+    var evaled = eval(code);
+
+    if (typeof evaled !== "string")
+    evaled = require("util").inspect(evaled);
+
+    message.channel.sendCode("xl", clean(evaled));
+  } catch(err) {
+    message.channel.sendMessage(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+  }
+
+}
 });
+
+
+function clean(text) {
+  if (typeof(text) === "string")
+  return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+  else
+  return text;
+}
 
 client.login(settings);
