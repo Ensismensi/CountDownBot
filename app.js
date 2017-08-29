f443d32b9fb11d1c5821eaaf15641a25544a36d7
Ensismensi <ensismensi1@gmail.com>
2017-08-29 17:04:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json').token;
 
-});
 client.on("guildMemberAdd",  (member) => {
   let guild = member.guild;
   guild.defaultChannel.sendMessage(`Welcome to ${guild.name}, we hope you ${member.user} have good time with us! Now we have ${guild.memberCount} members.`);
